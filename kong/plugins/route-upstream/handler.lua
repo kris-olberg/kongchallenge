e2b2d44fe5edf2a0430b92b220f29b987305c5ed
Kristine Olberg <kristineolberg@KristineMBP2020.home>
2020-08-03 14:49:22 -0500

    handler and schema plus others updated

--- kong/plugins/route-upstream/handler.lua
+++ kong/plugins/route-upstream/handler.lua
@@ -17,7 +17,7 @@ local plugin = {
   VERSION = "0.1",
 }
 
-
+local kong = kong
 
 -- do initialization here, any module level code runs in the 'init_by_lua_block',
 -- before worker processes are forked. So anything you add here will run once,
@@ -61,11 +61,38 @@ end --]]
 
 ---[[ runs in the 'access_by_lua_block'
 function plugin:access(plugin_conf)
+  plugin.super.access(self)
 
-  -- your custom code here
-  kong.log.inspect(plugin_conf)   -- check the logs for a pretty-printed config!
-  ngx.req.set_header(plugin_conf.request_header, "this is on a request")
+  local req_headers = kong.request.get_headers()
 
+  local match, upstream
+  local match_before, match_now = -1
+
+  for _, rule in ipairs(plugin_conf.rules) do
+    match = true
+    match_now = 0
+
+    for rule_header_name, rule_header_value in pairs(rule.headers) do
+      if req_headers[rule_header_name] ~= rule_header_value then
+        match = false
+        break
+      end
+
+      match_now = match_now + 1
+    end
+
+    if match then
+      upstream = match_now <= match_before and upstream or rule.upstream_name
+      match_before = match_now
+    end
+  end
+
+  kong.log.inspect(upstream)
+
+  if upstream then
+    kong.service.set_upstream(upstream)
+    kong.response.set_header("X-Upstream-Name", upstream)
+  end
 end --]]
 
 
