b7b9645355b2b1da32709e5bdff2c70d177a320a
Kristine Olberg <kristineolberg@KristineMBP2020.home>
2020-08-03 20:15:32 -0500

    rewording comments

--- kong/plugins/route-upstream/handler.lua
+++ kong/plugins/route-upstream/handler.lua
@@ -32,11 +32,11 @@ function plugin:access(plugin_conf)
   ---[[ get the headers and values; e.g, X-Country=Italy]]
   local req_headers = kong.request.get_headers()
 
-  ---[[ accesset local variables]]
+  ---[[ set local variables for logic eval, counters and the upstream name ]]
   local match, upstream
   local match_before, match_now = -1
 
-  ---[[ for each rule in config assume the request and rule headers match ]]
+  ---[[ for each rule in config rules assume the request and rule headers match ]]
   for _, rule in ipairs(plugin_conf.rules) do
     match = true
     match_now = 0
@@ -45,7 +45,7 @@ function plugin:access(plugin_conf)
     -- with each header value in the CURRENT rule ]]
     for rule_header_name, rule_header_value in pairs(rule.headers) do
       if req_headers[rule_header_name] ~= rule_header_value then
-        ---[[ if no match set local variable "match" to false and start again with the next header in the rule ]]
+        ---[[ if no match, set "match" to false and start again with the next header in the rule ]]
         match = false
         break
       end
@@ -57,7 +57,7 @@ function plugin:access(plugin_conf)
     ---[[ if match has not been set to false, set the upstream value ]]
     if match then
       ---[[ set upstream to upstream if the number of matches is less than or equal to match_before.
-      -- If no, set upstream to the upstream_name in the CURRENT rule. This has the effect of
+      -- If not, set upstream to the upstream_name in the CURRENT rule. This has the effect of
       -- selecting the upstream_name from the rule with the greatest number of header request/rule
       -- matches ]]
       upstream = match_now <= match_before and upstream or rule.upstream_name
