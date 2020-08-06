a9a9da0d3c73a9eaa4e3f8bef0bc97e57e4d9633
Kristine Olberg <kristineolberg@KristineMBP2020.home>
2020-08-05 21:06:20 -0500

    modding schema_spec and access_spec and removing set -e from shell script

--- spec/route-upstream/01-schema_spec.lua
+++ spec/route-upstream/01-schema_spec.lua
@@ -1,45 +1,92 @@
-local PLUGIN_NAME = "route-upstream"
+local schema_def = require("kong.plugins.route-upstream.schema")
+local v = require("spec.helpers").validate_plugin_config_schema
 
-
--- helper function to validate data against a schema
-local validate do
-  local validate_entity = require("spec.helpers").validate_plugin_config_schema
-  local plugin_schema = require("kong.plugins."..PLUGIN_NAME..".schema")
-
-  function validate(data)
-    return validate_entity(data, plugin_schema)
-  end
-end
-
-
-describe(PLUGIN_NAME .. ": (schema)", function()
-
-
-  it("accepts distinct request_header and response_header", function()
-    local ok, err = validate({
-        request_header = "My-Request-Header",
-        response_header = "Your-Response",
-      })
-    assert.is_nil(err)
-    assert.is_truthy(ok)
+describe("Plugin: route-upstream (schema)", function()
+  it("validates header name", function()
+    local config = {
+      rules = {
+        {
+          headers = {
+            ["X-Region!"] = "Italy",
+          },
+          upstream_name = "italy_cluster",
+        },
+      },
+    }
+    local ok, err = v(config, schema_def)
+    assert.falsy(ok)
+    assert.equal("bad header name 'X-Region!', allowed characters are A-Z, a-z, 0-9, '_', and '-'", err.config.rules[1].headers)
   end)
 
+  it("validates upstream", function()
+    local config = {
+      rules = {
+        {
+          headers = {
+            ["X-Country"] = "Italy",
+          },
+          upstream_name = "123",
+        },
+      },
+    }
+    local ok, err = v(config, schema_def)
+    assert.falsy(ok)
+    assert.equal("invalid value: 123", err.config.rules[1].upstream_name)
+    --print(require("spec.helpers").intercept(err.config.rules[1].upstream_name))
+  end)
 
-  it("does not accept identical request_header and response_header", function()
-    local ok, err = validate({
-        request_header = "they-are-the-same",
-        response_header = "they-are-the-same",
-      })
-
-    assert.is_same({
-      ["config"] = {
-        ["@entity"] = {
-          [1] = "values of these fields must be distinct: 'request_header', 'response_header'"
+  it("validates schema structure", function()
+    local config = {
+      rules = {
+        {
+          headers = {
+            ["X-Country"] = "Italy"
+          },
+          upstream_name = "italy_cluster"
         }
       }
-    }, err)
-    assert.is_falsy(ok)
+    }
+
+    local ok, err = v(config, schema_def)
+    assert.truthy(ok)
+    assert.is_nil(err)
   end)
 
+  it("validates required config object", function()
+    local config = nil
+    local ok, err = v(config, schema_def)
+    assert.falsy(ok)
+    assert.equal("required field missing", err.config.rules)
+  end)
 
+  it("validates required `rules` field", function()
+    local config = {}
+    local ok, err = v(config, schema_def)
+    assert.falsy(ok)
+    assert.equal("required field missing", err.config.rules)
+  end)
+
+  it("`rules` field must have at least one record", function()
+    local config = {
+      rules = {
+      }
+    }
+    local ok, err = v(config, schema_def)
+    assert.falsy(ok)
+    assert.equal("length must be at least 1", err.config.rules)
+  end)
+
+  it("rule record (headers and upstream_name) field missing", function()
+    local config = {
+      rules = {
+        {
+
+        }
+      }
+    }
+    local ok, err = v(config, schema_def)
+    assert.falsy(ok)
+    assert.equal("required field missing", err.config.rules[1].headers)
+    assert.equal("required field missing", err.config.rules[1].upstream_name)
+  end)
 end)
--- spec/route-upstream/02-access_spec.lua
+++ spec/route-upstream/02-access_spec.lua
@@ -1,28 +1,150 @@
 local helpers = require "spec.helpers"
+local version = require("version").version or require("version")
 
 
 local PLUGIN_NAME = "route-upstream"
+local KONG_VERSION = version(select(3, assert(helpers.kong_exec("version"))))
 
+local function prepare(bp)
+  local upstream1, upstream2, upstream3, upstream4, upstream5, upstream6
+  local service1
+
+  upstream1 = bp.upstreams:insert({
+    name = "europe_cluster",
+  })
+
+  upstream2 = bp.upstreams:insert({
+    name = "italy_cluster",
+  })
+
+  upstream3 = bp.upstreams:insert({
+    name = "rome_cluster",
+  })
+
+  upstream4 = bp.upstreams:insert({
+    name = "brazil_cluster",
+  })
+
+  upstream5 = bp.upstreams:insert({
+    name = "italy_cache_cluster",
+  })
+
+  upstream6 = bp.upstreams:insert({
+    name = "us_cluster",
+  })
+
+  bp.targets:insert({
+    upstream = upstream1,
+    target = "0.0.0.0:9005",
+  })
+
+  bp.targets:insert({
+    upstream = upstream2,
+    target = "0.0.0.0:9006",
+  })
+
+  bp.targets:insert({
+    upstream = upstream3,
+    target = "0.0.0.0:9007",
+  })
+
+  bp.targets:insert({
+    upstream = upstream4,
+    target = "0.0.0.0:9008",
+  })
+
+  bp.targets:insert({
+    upstream = upstream5,
+    target = "0.0.0.0:9009",
+  })
+
+  bp.targets:insert({
+    upstream = upstream6,
+    target = "0.0.0.0:9010",
+  })
+
+  -- default: requests to service1 will be sent to europe_cluster
+  service1 = bp.services:insert({
+    name = "service1",
+    host = "europe_cluster",
+    protocol = "http",
+  })
+
+  -- requests that match route "/local" will be proxied to europe_cluster
+  bp.routes:insert({
+    paths = { "/local", },
+    service = service1,
+  })
+
+  bp.plugins:insert {
+    name = PLUGIN_NAME,
+    config = {
+      rules = {
+        {
+          headers = {
+            ["X-Country"] = "Italy",
+          },
+          upstream_name = "europe_cluster",
+        },
+        {
+          headers = {
+            ["X-Country"] = "Italy",
+            ["X-Regione"] = "Abruzzo",
+          },
+          upstream_name = "italy_cluster",
+        },
+        {
+          headers = {
+            ["X-Country"] = "Italy",
+            ["X-Regione"] = "Rome",
+          },
+          upstream_name = "rome_cluster",
+        },
+        {
+          headers = {
+            ["X-Country"] = "Brazil",
+            ["X-Regione"] = "Goiania",
+          },
+          upstream_name = "brazil_cluster",
+        },
+        {
+          headers = {
+            ["X-Country"] = "Italy",
+            ["Connection"] = "close",
+          },
+          upstream_name = "italy_cache_cluster",
+        },
+        {
+          headers = {
+            ["Accept-Language"] = "en-us",
+          },
+          upstream_name = "us_cluster",
+        },
+      },
+    },
+  }
+end
 
 for _, strategy in helpers.each_strategy() do
   describe(PLUGIN_NAME .. ": (access) [#" .. strategy .. "]", function()
     local client
 
     lazy_setup(function()
+      local bp
 
-      local bp = helpers.get_db_utils(strategy, nil, { PLUGIN_NAME })
-
-      -- Inject a test route. No need to create a service, there is a default
-      -- service which will echo the request.
-      local route1 = bp.routes:insert({
-        hosts = { "test1.com" },
-      })
-      -- add the plugin to test to the route we created
-      bp.plugins:insert {
-        name = PLUGIN_NAME,
-        route = { id = route1.id },
-        config = {},
-      }
+      if KONG_VERSION >= version("0.15.0") then
+        --
+        -- Kong version 0.15.0/1.0.0, new test helpers
+        --
+        bp = helpers.get_db_utils(strategy, nil, { PLUGIN_NAME })
+        prepare(bp)
+      else
+        --
+        -- Pre Kong version 0.15.0/1.0.0, older test helpers
+        --
+        bp = helpers.get_db_utils(strategy)
+        prepare(bp)
+      end
 
       -- start kong
       assert(helpers.start_kong({
@@ -30,8 +152,9 @@ for _, strategy in helpers.each_strategy() do
         database   = strategy,
         -- use the custom test template to create a local mock server
         nginx_conf = "spec/fixtures/custom_nginx.template",
-        -- make sure our plugin gets loaded
-        plugins = "bundled," .. PLUGIN_NAME,
+        -- set the config item to make sure our plugin gets loaded
+        plugins = "bundled," .. PLUGIN_NAME,  -- since Kong CE 0.14
+        custom_plugins = PLUGIN_NAME,         -- pre Kong CE 0.14
       }))
     end)
 
@@ -49,38 +172,126 @@ for _, strategy in helpers.each_strategy() do
 
 
 
-    describe("request", function()
-      it("gets a 'hello-world' header", function()
-        local r = client:get("/request", {
+    --describe("request", function()
+    --  it("gets a 'X-Upstream-Name' header", function()
+    --    local r = assert(client:send {
+    --      method = "GET",
+    --      path = "/local",
+    --      headers = {
+    --        ["X-Country"] = "Italy",
+    --      }
+    --    })
+    --    -- validate that the request succeeded, response status 200
+    --    assert.response(r).has.status(200)
+    --    -- now check the request (as echoed by mockbin) to have the header
+    --    local header_value = assert.request(r).has.header("X-Upstream-Name")
+    --    -- validate the value of that header
+    --    assert.equal("europe_cluster", header_value)
+    --  end)
+    --end)
+
+
+
+    describe("Upstream response", function()
+      it("checks if request with {X-Country = Italy} only must match rule with {X-Country = Italy} only", function()
+        local r = assert(client:send {
+          method = "GET",
+          path = "/local",
           headers = {
-            host = "test1.com"
-          }
+            ["X-Country"] = "Italy",
+          },
         })
         -- validate that the request succeeded, response status 200
         assert.response(r).has.status(200)
         -- now check the request (as echoed by mockbin) to have the header
-        local header_value = assert.request(r).has.header("hello-world")
+        local header_value = assert.response(r).has.header("X-Upstream-Name")
         -- validate the value of that header
-        assert.equal("this is on a request", header_value)
+        assert.equal("europe_cluster", header_value)
       end)
-    end)
 
-
-
-    describe("response", function()
-      it("gets a 'bye-world' header", function()
-        local r = client:get("/request", {
+      it("checks if request with {X-Country = Italy, X-Regione = Abruzzo} matches rule with {X-Country = Italy, X-Regione = Abruzzo}", function()
+        local r = assert(client:send {
+          method = "GET",
+          path = "/local",
           headers = {
-            host = "test1.com"
-          }
+            ["X-Country"] = "Italy",
+            ["X-Regione"] = "Abruzzo",
+          },
         })
         -- validate that the request succeeded, response status 200
         assert.response(r).has.status(200)
-        -- now check the response to have the header
-        local header_value = assert.response(r).has.header("bye-world")
+        -- now check the request (as echoed by mockbin) to have the header
+        local header_value = assert.response(r).has.header("X-Upstream-Name")
         -- validate the value of that header
-        assert.equal("this is on the response", header_value)
+        assert.equal("italy_cluster", header_value)
       end)
+
+      it("checks if request with {X-Country = Italy, X-Regione = Rome} matches rule with {X-Country = Italy, X-Regione = Rome}", function()
+        local r = assert(client:send {
+          method = "GET",
+          path = "/local",
+          headers = {
+            ["X-Country"] = "Italy",
+            ["X-Regione"] = "Rome",
+          },
+        })
+        -- validate that the request succeeded, response status 200
+        assert.response(r).has.status(200)
+        -- now check the request (as echoed by mockbin) to have the header
+        local header_value = assert.response(r).has.header("X-Upstream-Name")
+        -- validate the value of that header
+        assert.equal("rome_cluster", header_value)
+      end)
+    end)
+
+    it("checks if request with {X-Country = Brazil, X-Regione = Goiania} matches rule with {X-Country = Brazil, X-Regione = Goiania}", function()
+      local r = assert(client:send {
+        method = "GET",
+        path = "/local",
+        headers = {
+          ["X-Country"] = "Brazil",
+          ["X-Regione"] = "Goiania",
+        },
+      })
+      -- validate that the request succeeded, response status 200
+      assert.response(r).has.status(200)
+      -- now check the request (as echoed by mockbin) to have the header
+      local header_value = assert.response(r).has.header("X-Upstream-Name")
+      -- validate the value of that header
+      assert.equal("brazil_cluster", header_value)
+    end)
+
+    it("checks if request with {X-Country = Italy, Connection = close} matches rule with {X-Country = Italy, Connection = close}", function()
+      local r = assert(client:send {
+        method = "GET",
+        path = "/local",
+        headers = {
+          ["X-Country"] = "Italy",
+          ["Connection"] = "close",
+        },
+      })
+      -- validate that the request succeeded, response status 200
+      assert.response(r).has.status(200)
+      -- now check the request (as echoed by mockbin) to have the header
+      local header_value = assert.response(r).has.header("X-Upstream-Name")
+      -- validate the value of that header
+      assert.equal("italy_cache_cluster", header_value)
+    end)
+
+    it("checks if request with {Accept-Language = en-us} matches rule with {Accept-Language = en-us}", function()
+      local r = assert(client:send {
+        method = "GET",
+        path = "/local",
+        headers = {
+          ["Accept-Language"] = "en-us",
+        },
+      })
+      -- validate that the request succeeded, response status 200
+      assert.response(r).has.status(200)
+      -- now check the request (as echoed by mockbin) to have the header
+      local header_value = assert.response(r).has.header("X-Upstream-Name")
+      -- validate the value of that header
+      assert.equal("us_cluster", header_value)
     end)
 
   end)
